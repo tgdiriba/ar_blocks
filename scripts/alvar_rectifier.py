66b4cfa317caa2ca1026fc84271b1c70b04b385d
Tigist Diriba <tgdiriba@gmail.com>
2014-06-25 15:01:19 -0500

    Rectification for AR

--- scripts/alvar_rectifier.py
+++ scripts/alvar_rectifier.py
@@ -2,16 +2,19 @@
 
 import rospy
 from ar_track_alvar.msg import AlvarMarkers, AlvarMarker
+from visualization_msgs.msg import MarkerArray
 
 class AlvarRectifier():
 
 	def __init__(self):
-		self.X_OFFSET, self.Y_OFFSET, self.Z_OFFSET = float(0.0), float(0.0), float(0.0)
+		self.X_OFFSET, self.Y_OFFSET, self.Z_OFFSET = float(10.5), float(10.5), float(10.5)
 		self.rectified_response = AlvarMarkers()
+		self.visual_marker = MarkerArray()
 		rospy.init_node('nxr_ar_rectifier')
 		rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.rectify_markers)
+		self.visual_marker_pub = rospy.Publisher('nxr_visualization_marker', Marker)
 		self.ar_pub = rospy.Publisher('nxr_ar_rectifier', AlvarMarkers)								
-		self.ar_rectifier()	
+		self.ar_rectifier()
 
 	def rectify_markers(self, response):
 		for marker in response.markers:
@@ -20,12 +23,14 @@ class AlvarRectifier():
 			marker_pose.position.y += self.Y_OFFSET
 			marker_pose.position.z += self.Z_OFFSET
 		
+		# Pick the first marker and publish
 		self.rectified_response = response
 	
 	def ar_rectifier(self):
 		self.ar_rate = rospy.Rate(100)
 		while not rospy.is_shutdown():
 			self.ar_pub.publish(self.rectified_response)
+			self.visual_marker_pub.publish(self.visual_marker);
 			self.ar_rate.sleep()
 
 if __name__ == '__main__':
--- src/ARWorldBuilder.cpp
+++ src/ARWorldBuilder.cpp
@@ -8,20 +8,21 @@ using namespace std;
 static const float g_table_dimensions[3] = { 0.608012, 1.21602, 0.60325 };
 // static const float g_table_position[3] = { 0.608012, 1.21602, 0.60325 };
 
-void ARWorldBuilder::pickLargest()
+bool ARWorldBuilder::pickLargest()
 {
 	pthread_mutex_lock(&ar_blocks_mutex_);
 	// Find the largest block
-	ARBlock& largest_block;
 	map<unsigned int, ARBlock>::iterator it = ar_blocks_.begin();
 	map<unsigned int, ARBlock>::iterator end = ar_blocks_.end();
 	if(it != end) {
-		largest_block = 
+		ARBlock& largest_block = it->second; 
 		for( ; it != ar_blocks_.end(); it++) {
-			if
+			
 		}
 	}
 	pthread_mutex_unlock(&ar_blocks_mutex_);
+
+	return false;
 }
 
 ARWorldBuilder::ARWorldBuilder(unsigned int cutoff) : cutoff_confidence_(cutoff)
